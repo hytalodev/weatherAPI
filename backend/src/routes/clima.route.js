0a161a4ffb793014051d5c79b20c055240a3fdc6
Hytalo Lima <hytalodev@gmail.com>
2025-07-09 11:57:23 -0300

    :recycle: refactor: Update in code

---
+++ backend/src/routes/clima.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import climaController from "../controllers/clima.Controllers.js";
+
+const city = Router();
+
+// Rotas utilizadas da API
+city.get("/", climaController.listaCidades);
+city.get("/pesquisa/:nome", climaController.pesquisaCidade); 
+city.get("/agradaveis", climaController.cidadesAgradaveis);
+city.get("/quentes", climaController.cidadesQuentes);
+
+export default city;
